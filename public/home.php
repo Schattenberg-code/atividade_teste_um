2cd4de31e69e8af5df402dabc421e6350a90b324
schattenberg-code <lucas_schattenberg@estudante.sesisenai.org.br>
2026-06-05 13:23:49 -0300

    refactor: estrutura do html, organização, visual e comentários

--- public/home.php
+++ public/home.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION["usuario"])) {
 }
 
 
-//inserir novo usuario
+//---------inserir novo usuario----------
 
 if (isset($_POST["cadastrar"])) {
 
@@ -26,9 +26,9 @@ if (isset($_POST["cadastrar"])) {
 }
 /* Recebe os dados do formulário e cadastra um novo usuário no banco de dados. */
 
+//----------------------------------------
 
-
-//deletar usuario
+//--------------deletar usuario------------
 
 if (isset($_POST["deletar"])) {
 
@@ -42,9 +42,9 @@ if (isset($_POST["deletar"])) {
     }
 }
 
-//-----------------------------
+//-----------------------------------------
 
-//editar usuario
+//--------------editar usuario-------------
 
 if (isset($_POST["editar"])) {
 
@@ -61,16 +61,11 @@ if (isset($_POST["editar"])) {
     }
 }
 
-//----------------
-
+//---------------------------------------------
 
 ?>
 
-
-
-
-
-<!--   -----------------------html-----------------------    -->
+<!--------------------html--------------------->
 
 <html lang="en">
 
@@ -86,99 +81,89 @@ if (isset($_POST["editar"])) {
 <body>
 
     <section id="page-section">
+        <div class=" div-conteudo shadow-lg rounded rounded-3 w-75 container-xxl">
 
-        <div class=" div-conteudo shadow-lg rounded rounded-3 w-75 container-sm">
-
-            <h1>bem vindo</h1>
-            <p>usuario logado: <?php echo $_SESSION["usuario"]; ?> <br></p>
-
-            <a href="logout.php">sair</a>
-
-            <p class="h2 pb-3 d-flex justify-content-center">Cadastro novo usuário</p>
+            <div>
+                <h1 class="fs-1 text-center">bem vindo</h1>
+                <p class="text-center">usuario logado: <?php echo $_SESSION["usuario"]; ?> <br></p>
+                <a class="btn btn-outline-danger d-flex justify-content-center" href="logout.php">sair</a>
+                <hr>
+            </div>
 
             <form method="POST" id="form-adicionar-usuario">
-                <label class="form-label fw-bold" for="usuario">Úsuario:</label>
-                <input class="form-control" type="text" name="usuario">
-                <br>
-                <label class="form-label fw-bold" for="senha">Senha:</label>
-                <input class="form-control" type="password" name="senha">
-                <br>
-                <br>
-                <button class="btn btn-primary btn-lg w-100" name="cadastrar" type="submit">Cadastrar</button>
+                <p class="h2 pb-3 d-flex justify-content-center mt-5">Cadastro novo usuário</p>
+                <div class="w-50 w-50 mx-auto">
+                    <label class="form-label fw-bold " for="usuario">Úsuario:</label>
+                    <input class="form-control" type="text" name="usuario">
+                </div>
+                <div class="w-50 w-50 mx-auto">
+                    <label class="form-label fw-bold" for="senha">Senha:</label>
+                    <input class="form-control" type="password" name="senha">
+                </div>
+                <div class="d-flex justify-content-center mt-5">
+                    <button class="btn btn-primary w-25" name="cadastrar" type="submit">Cadastrar</button>
+                </div>
             </form>
 
+            <hr>
 
-
-
-<br>
             <form method="POST">
-
-                <p class="h2 pb-3 d-flex justify-content-center">Excluir usuário</p>
-
-                <label class="form-label fw-bold" for="excluirUsuario">Selecione qual usuario deseja excluir</label>
-                <select class="form-select" name="usuario_id" id="excluirUsuario">
-
-                    <?php
-                    $sql = "SELECT id, usuario FROM usuario";
-                    $resultado = $conn->query($sql);
-
-                    while ($linha = $resultado->fetch_assoc()) {
-                        echo "<option value='{$linha['id']}'>
-                    {$linha['usuario']}
-                  </option>";
-                    }
-                    ?>
-
-                </select>
-                    <br>
-                    <br>
-                <button class="btn btn-primary btn-lg w-100" name="deletar" type="submit">Excluir</button>
+                <p class="h2 pb-3 d-flex justify-content-center mt-5">Excluir usuário</p>
+                <div class="w-50 w-50 mx-auto">
+                    <label class="form-label fw-bold" for="excluirUsuario">Selecione qual usuario deseja excluir</label>
+                    <select class="form-select ms-0" name="usuario_id" id="excluirUsuario">
+                        <?php
+                        $sql = "SELECT id, usuario FROM usuario";
+                        $resultado = $conn->query($sql);
+                        while ($linha = $resultado->fetch_assoc()) {
+                            echo "<option value='{$linha['id']}'>{$linha['usuario']}</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div class="d-flex justify-content-center mt-5">
+                    <button class="btn btn-primary w-25" name="deletar" type="submit">Excluir</button>
+                </div>
             </form>
 
+            <hr>
 
+            <form method="POST">
+                <p class="h2 pb-3 d-flex justify-content-center mt-5">Editar usuário</p>
+                <div class="w-50 w-50 mx-auto">
+                    <label class="form-label fw-bold" for="editarUsuario"> Selecione um usuario para editar </label>
+                    <select class="form-select ms-0" name="usuario_id" id="editarUsuario">
+                        <?php
+                        $sql = "SELECT id, usuario FROM usuario";
+                        $resultado = $conn->query($sql);
 
-
-          <br>  <form method="POST">
-
-                <p class="h2 pb-3 d-flex justify-content-center">Editar usuário</p>
-
-                <label class="form-label fw-bold" for="editarUsuario"> Selecione um usuario para editar </label>
-                <select class="form-select" name="usuario_id" id="editarUsuario">
-
-                    <?php
-                    $sql = "SELECT id, usuario FROM usuario";
-                    $resultado = $conn->query($sql);
-
-                    while ($linha = $resultado->fetch_assoc()) {
-                        echo "<option value='{$linha['id']}'>
-                    {$linha['usuario']}
-                  </option>";
-                    }
-                    ?>
-
-                </select>
-
-                <br>
-                <label class="form-label fw-bold" for="">nome</label>
-                <input class="form-control form-control-lg mb-4" type="text" name="editarNome">
-                <br>
-                <label class="form-label fw-bold" for="">Senha</label>
-                <input class="form-control form-control-lg mb-4" type="text" name="editarSenha">
-
-                <button class="btn btn-primary btn-lg w-100" type="submit" name="editar">Editar</button>
-
+                        while ($linha = $resultado->fetch_assoc()) {
+                            echo "<option value='{$linha['id']}'>{$linha['usuario']}</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div class="w-50 w-50 mx-auto">
+                    <label class="form-label fw-bold" for="">nome</label>
+                    <input class="form-control" type="text" name="editarNome">
+                </div>
+                <div class="w-50 w-50 mx-auto">
+                    <label class="form-label fw-bold" for="">Senha</label>
+                    <input class="form-control" type="text" name="editarSenha">
+                </div>
+                <div class="d-flex justify-content-center mt-5">
+                    <button class="btn btn-primary w-25" name="deletar" type="editar">Editar</button>
+                </div>
             </form>
 
             <?php
             include("../public/component/table.php"); //chama a tabela.php para aparecer na home.php sem a necessidade de escrever todo o códigoda tabela novamente.
             ?>
+
         </div>
     </section>
-
-
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
     crossorigin="anonymous"></script>
-
 </html>
